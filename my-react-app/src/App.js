2d520faa701fc69b93ed67792cd1a68b2a174eff
Studggle <vg1992005@gmail.com>
2025-11-23 23:25:40 +0530

    Add Navbar component with styling
    Introduced a new Navbar component and its CSS for site navigation. Integrated the Navbar into App.js to provide a consistent navigation bar across the application.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import Navbar from './Navbar';  
 import './App.css';
 import car1 from './assets/car1.png';
 import car2 from './assets/car2.png';
@@ -8,6 +9,7 @@ import car3 from './assets/car3.png';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Vehicle Display App</h1>
 
       <div className="card-container">
---
+++ my-react-app/src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo"></div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Vehicles</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
